5e9593aa35990bdc02b429dbc08aabcbab6bed4a
Dennis Gonzales <pro.gonzalesdennis@gmail.com>
2023-07-13 00:46:42 +0800

    feat(cache): add shared prefs

---
+++ app/src/main/java/com/dnnsgnzls/mvvm/util/Prefs.kt
@@ -0,0 +1,37 @@
+package com.dnnsgnzls.mvvm.util
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.core.content.edit
+
+class Prefs {
+
+    companion object {
+
+        private const val PREF_SAVE_TIME = "pref_save_time"
+        private var prefs: SharedPreferences? = null
+
+        @Volatile
+        private var instance: Prefs? = null
+
+        operator fun invoke(context: Context): Prefs = instance ?: synchronized(this) {
+            instance ?: buildHelper(context).also {
+                instance = it
+            }
+        }
+
+        private fun buildHelper(context: Context): Prefs {
+            val sharedPreferencesName = "${context.packageName}_preferences"
+            prefs = context.getSharedPreferences(sharedPreferencesName, Context.MODE_PRIVATE)
+            return Prefs()
+        }
+    }
+
+    fun saveUpdateTime(time: Long) {
+        prefs?.edit(commit = true) { putLong(PREF_SAVE_TIME, time) }
+    }
+
+    fun getUpdateTime() = prefs?.getLong(PREF_SAVE_TIME, 0) ?: 0
+
+    fun getCacheDuration() = prefs?.getString("pref_cache_duration", "")
+}
--- app/src/main/java/com/dnnsgnzls/mvvm/viewmodel/BaseViewModel.kt
+++ app/src/main/java/com/dnnsgnzls/mvvm/viewmodel/BaseViewModel.kt
@@ -7,6 +7,18 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlin.coroutines.CoroutineContext
 
+/**
+ * Don't pass a context or activity to a ViewModel in Android's MVVM architecture.
+ *
+ * This avoids potential memory leaks and adheres to the ViewModel's purpose: to be lifecycle-aware and not hold references to views, like Activities or Fragments.
+ * A ViewModel can outlive these views, which can cause issues if it directly references them.
+ *
+ * If a context is necessary, such as for database access, use the Application Context.
+ *
+ *
+ * This global context isn't tied to a specific lifecycle. The subclass AndroidViewModel provides this, as it includes an Application reference.
+ * This ensures the ViewModel is tied to the application's lifecycle, not an activity's. With this, you get an application-scoped database, no context leaks, and no need for context handling in your ViewModel."
+ */
 abstract class BaseViewModel(application: Application): AndroidViewModel(application), CoroutineScope {
 
     private val job = Job()
