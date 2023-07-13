d833a8dd456cf2dd261c8e1b10652a33fc1cf520
Dennis Gonzales <pro.gonzalesdennis@gmail.com>
2023-07-13 16:00:30 +0800

    feat(navigation): add navigation to MainActivity
    - Added `NavController` property to `MainActivity`
    - Initialized `NavController` in `onPostCreate()` method
    - Set up action bar with `NavController` in `onPostCreate()` method
    - Implemented `onSupportNavigateUp()` method for navigating up
    
    refactor(layout): rename fragmentContainerView to nav_fragment_container
    
    - Renamed the id of the fragment container view in activity_main.xml to "nav_fragment_container"
    
    docs(navigation): update labels in hero_navigation.xml
    
    - Updated the label of the heroFragment to "Hero"
    - Updated the label of the detailsFragment to "Hero Details"

--- app/src/main/java/com/dnnsgnzls/jetpack/views/MainActivity.kt
+++ app/src/main/java/com/dnnsgnzls/jetpack/views/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.dnnsgnzls.jetpack.views
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.dnnsgnzls.jetpack.R
 
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        navController = findNavController(this, R.id.nav_fragment_container)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
