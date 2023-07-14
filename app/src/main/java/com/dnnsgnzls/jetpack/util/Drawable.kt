57147efe7518df040b34faacc39f729b891e5b5e
Dennis Gonzales <pro.gonzalesdennis@gmail.com>
2023-07-14 16:41:22 +0800

    refactor: drawable extract params

--- app/src/main/java/com/dnnsgnzls/jetpack/util/Drawable.kt
+++ app/src/main/java/com/dnnsgnzls/jetpack/util/Drawable.kt
@@ -1,12 +1,24 @@
 package com.dnnsgnzls.jetpack.util
 
 import android.content.Context
+import android.graphics.Color
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.createBitmap
+import androidx.core.graphics.drawable.toBitmap
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.dnnsgnzls.jetpack.R
 
-fun getProgressDrawable(context: Context): CircularProgressDrawable {
+fun getProgressDrawable(
+    context: Context,
+    strokeWidth: Float = 10f,
+    centerRadius: Float = 50f,
+    colorResId: Int = R.color.color_accent
+): CircularProgressDrawable {
+    val color = ContextCompat.getColor(context, colorResId)
     return CircularProgressDrawable(context).apply {
-        strokeWidth = 10f
-        centerRadius = 50f
+        this.strokeWidth = strokeWidth
+        this.centerRadius = centerRadius
+        setColorSchemeColors(color)
         start()
     }
 }
